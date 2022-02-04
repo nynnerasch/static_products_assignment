4fa296ab4d44f9711941830937798a497518ebcb
nynnerasch <nynne.andersen@yahoo.dk>
2022-02-04 12:57:21 +0100

    making it a little dynamic

---
+++ product.js
@@ -0,0 +1,20 @@
+const url = "https://kea-alt-del.dk/t7/api/products/2801";
+//fetch the data
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+//populate the page
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".brand").textContent = product.brandname;
+  document.querySelector(".productName").textContent =
+    product.productdisplayname;
+  document.querySelector("p.productName").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    "img.productimage"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  document.querySelector("img.productimage").alt = product.productdisplayname;
+}
